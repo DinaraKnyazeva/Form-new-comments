aa90a69841f05942c041860756189561486fe637
DinaraKnyazeva <Dinarka470@mail.ru>
2023-09-19 21:34:02 +0400

    created form: button, textarea

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import "./form.css";
+
+export default function Form() {
+  return (
+    <>
+      <div className="form">
+        <textarea className="textarea"></textarea>
+        <button className="button">Оставить комментарий</button>
+      </div>
+    </>
+  );
+}
