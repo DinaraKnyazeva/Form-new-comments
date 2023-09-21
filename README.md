22bd952de5875140145348176d521003ddc40927
DinaraKnyazeva <Dinarka470@mail.ru>
2023-09-21 16:14:19 +0400

    added function onChange,spamFilter,onSubmit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Getting Started with Create React App
 
+<img src="src/components/screen.png">
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,54 @@
+import React, { useState } from "react";
 import "./form.css";
 
 export default function Form() {
+  const [text, setText] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const spamFilter = (str) => {
+    const spamWords = ["viagra", "xxx"];
+    return str.replace(
+      new RegExp(`\\b(${spamWords.join("|")})\\b`, "gi"),
+      "***"
+    );
+  };
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (text.trim() !== "") {
+      const filteredText = spamFilter(text);
+      setComments([
+        { text: filteredText, isNew: true },
+        ...comments.map((c) => ({ ...c, isNew: false })),
+      ]);
+      setText("");
+    }
+  };
+
   return (
-    <>
-      <div className="form">
-        <textarea className="textarea"></textarea>
-        <button className="button">Оставить комментарий</button>
+    <div className="form">
+      <h1>ЧАТ</h1>
+      <div className="result">
+        {comments.map((comment, index) => (
+          <div
+            key={index}
+            className={comment.isNew ? "comment new-comment" : "comment"}
+          >
+            {comment.text}
+          </div>
+        ))}
       </div>
-    </>
+      <textarea
+        className="textarea"
+        onChange={onChange}
+        value={text}
+      ></textarea>
+      <button className="button" onClick={onSubmit}>
+        Оставить комментарий
+      </button>
+    </div>
   );
 }
